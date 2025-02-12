e634623a06794d14bf764b6fb6c5916b98266603
giandinhthai <giandinhthai@gmail.com>
2025-02-12 20:28:10 +0700

    search product using elasticsearch

---
+++ search-service/src/main/java/com/ecommerce/search_service/model/entity/ProductDocument.java
@@ -0,0 +1,42 @@
+package com.ecommerce.search_service.model.entity;
+
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(indexName = "products")
+public class ProductDocument {
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String name;
+
+    @Field(type = FieldType.Text)
+    private String brand;
+
+    @Field(type = FieldType.Text)
+    private String cover;
+
+    @Field(type = FieldType.Text)
+    private String description;
+
+    @Field(type = FieldType.Double)
+    private Double price;
+
+    @Field(type = FieldType.Integer)
+    private Integer quantity;
+
+    @Field(type = FieldType.Double)
+    private Double rate;
+
+    @Field(type = FieldType.Keyword)
+    private String categoryName;
+
+}
