99df7eecd2d5ed96c8cf19d4a1ff2653e459537c
giandinhthai <giandinhthai@gmail.com>
2025-02-12 20:28:10 +0700

    search product using elasticsearch

--- search-service/src/main/java/com/ecommerce/search_service/service/SearchService.java
+++ search-service/src/main/java/com/ecommerce/search_service/service/SearchService.java
@@ -3,6 +3,7 @@ package com.ecommerce.search_service.service;
 
 
 import com.ecommerce.search_service.model.entity.Product;
+import com.ecommerce.search_service.model.entity.ProductDocument;
 import com.ecommerce.search_service.model.response.ProductResponse;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.data.domain.Page;
@@ -12,4 +13,5 @@ import java.util.List;
 
 public interface SearchService {
     Page<ProductResponse> searchProducts(String name, String category, Double minPrice, Double maxPrice, Double minRate, Double maxRate, Pageable pageable);
+    Page<ProductDocument> elasticSearchProducts(String name, String category, Double minPrice, Double maxPrice, Double minRate, Double maxRate, Pageable pageable);
 }
