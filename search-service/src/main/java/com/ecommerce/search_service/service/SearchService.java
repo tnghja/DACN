26072741135b5b665b8ab0bc92cba27193da8a32
giandinhthai <giandinhthai@gmail.com>
2025-02-12 15:37:33 +0700

    search product using jpa Specification

--- search-service/src/main/java/com/ecommerce/search_service/service/SearchService.java
+++ search-service/src/main/java/com/ecommerce/search_service/service/SearchService.java
@@ -3,7 +3,6 @@ package com.ecommerce.search_service.service;
 
 
 import com.ecommerce.search_service.model.entity.Product;
-import com.ecommerce.search_service.model.entity.ProductDocument;
 import com.ecommerce.search_service.model.response.ProductResponse;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.data.domain.Page;
@@ -13,5 +12,4 @@ import java.util.List;
 
 public interface SearchService {
     Page<ProductResponse> searchProducts(String name, String category, Double minPrice, Double maxPrice, Double minRate, Double maxRate, Pageable pageable);
-    Page<ProductDocument> elasticSearchProducts(String name, String category, Double minPrice, Double maxPrice, Double minRate, Double maxRate, Pageable pageable);
 }
