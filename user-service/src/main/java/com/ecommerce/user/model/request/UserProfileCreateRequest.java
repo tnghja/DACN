6107210c26bfcc31b75182ae8938cf7ccbc8c009
giandinhthai <giandinhthai@gmail.com>
2025-03-13 09:34:18 +0700

    Reverted folder1 and folder2 to commit 5f11668c

--- user-service/src/main/java/com/ecommerce/user/model/request/UserProfileCreateRequest.java
+++ user-service/src/main/java/com/ecommerce/user/model/request/UserProfileCreateRequest.java
@@ -2,33 +2,27 @@ package com.ecommerce.user.model.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserProfileCreateRequest {
-
+    String userId;
 
     @Email(message = "INVALID_EMAIL")
     @NotBlank(message = "EMAIL_REQUIRED")
-    @Size(max = 255, message = "EMAIL_TOO_LONG")
     String email;
 
-    @Size(min = 4, max = 50, message = "USERNAME_INVALID")
+    @Size(min = 4, message = "USERNAME_INVALID")
     @NotBlank(message = "USERNAME_REQUIRED")
     String userName;
 
-    @NotBlank(message = "PASSWORD_REQUIRED")
-    @Size(min = 8, message = "PASSWORD_TOO_SHORT")
     String password;
 
-    @NotBlank(message = "NAME_REQUIRED")
-    @Size(max = 100, message = "NAME_TOO_LONG")
-    String name;
+    String name = "1111";
 }
