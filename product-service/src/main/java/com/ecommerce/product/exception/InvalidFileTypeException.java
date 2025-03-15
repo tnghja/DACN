7ee062a29088a7c702f84b0ef3de05529a0b9c2b
tnghja <nghia.nannt@gmail.com>
2025-03-15 10:43:34 +0700

    Add Exception

---
+++ product-service/src/main/java/com/ecommerce/product/exception/InvalidFileTypeException.java
@@ -0,0 +1,9 @@
+package com.ecommerce.product.exception;
+
+public class InvalidFileTypeException extends RuntimeException{
+
+        public InvalidFileTypeException(String message) {
+            super(message);
+        }
+}
+
