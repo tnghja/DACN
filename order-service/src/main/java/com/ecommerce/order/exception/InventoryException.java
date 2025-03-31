298b0677ade17824b6e49b8d277978d85c209365
tnghja <nghia.nannt@gmail.com>
2025-03-31 16:34:44 +0700

    Fix Upload, Add product sync, Add rating, Fix order

---
+++ order-service/src/main/java/com/ecommerce/order/exception/InventoryException.java
@@ -0,0 +1,7 @@
+package com.ecommerce.order.exception;
+
+public class InventoryException extends RuntimeException {
+    public InventoryException(String message) {
+        super(message);
+    }
+}
